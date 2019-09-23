20751b5924057d8ead192adfc8362bd8cb59829f
mirrordust <liaoshanhe@gmail.com>
2019-09-23 21:59:06 +0800

    rm token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,10 @@
 
 deploy_path=$1
 secret_key=$2
+upload_token=$3
 
 project_path="${deploy_path}/jianmu"
 
 chmod +x "${project_path}/bin/jianmu"
 
-nohup "${project_path}/bin/jianmu" -Dconfig.resource=deploy.conf -Dplay.http.secret.key=${secret_key} -Dhttp.port=55555 > /dev/null 2>&1 &
+nohup "${project_path}/bin/jianmu" -Dconfig.resource=deploy.conf -Dplay.http.secret.key=${secret_key} -Dupload-dataset.token=${upload_token} -Dhttp.port=55555 > /dev/null 2>&1 &
